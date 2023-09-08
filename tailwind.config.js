d14d4b8dcc9017c53da24d8d2cda4fb4e981e967
Marina Del Conte <marinadelconte@yahoo.com>
2023-09-08 11:53:50 -0400

    removed tailwind and added bulma

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./views/**/*.{html,handlebars,js}", "./public/js/**/*.{html,handlebars,js}"], 
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
